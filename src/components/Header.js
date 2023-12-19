70a2acf72bc4e9d99462ce0d089e2a6ca1e08a84
phurisk <phuris.k@kkumail.com>
2023-12-19 21:42:12 +0700

    now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,27 @@
+
+
 // import React, { useState } from "react";
 // import { Link } from "react-scroll";
 
-// const Header = () => {
-  
 
+
+// const Header = () => {
 //   return (
-    
-//     <header className="bg-gray-800 py-4 px-8">
+//     <header  className="py-5 px- rounded-xl   bg-[#04192f] mr-4">
 //       <nav className="flex items-center justify-between">
 //         <div className="flex items-center">
 //           <Link
 //             activeClass="text-yellow-400"
-//             to="about"
+//             to="*"
 //             spy={true}
 //             smooth={true}
 //             offset={-70}
 //             duration={500}
 //           >
-//             <span className="text-white text-2xl font-bold">Phuris</span>
+//             <span className="text-white text-4xl font-bold font-rubik ml-4">Phuris</span>
 //           </Link>
 //         </div>
-//         <div className="flex items-center space-x-4">
+//         <div className="flex items-center space-x-7 ">
 //           <Link
 //             activeClass="text-yellow-400"
 //             to="about"
@@ -29,54 +30,70 @@
 //             offset={-70}
 //             duration={500}
 //           >
-//             About
+//             <button className="font-rubik hover:text-[#54d2d2] text-xl">About</button>
 //           </Link>
 //           <Link
 //             activeClass="text-yellow-400"
-//             to="work"
+//             to="skills"
 //             spy={true}
 //             smooth={true}
 //             offset={-70}
 //             duration={500}
 //           >
-//             Work
+//             <button className="font-rubik hover:text-[#ffcb00] text-xl">Skills</button>
 //           </Link>
-//           <Link
-//             activeClass="text-yellow-400"
-//             to="testimonials"
-//             spy={true}
-//             smooth={true}
-//             offset={-70}
-//             duration={500}
-//           >
-//             Contact
-//           </Link>
+          // <Link
+          //   activeClass="text-yellow-400"
+          //   to="work"
+          //   spy={true}
+          //   smooth={true}
+          //   offset={-70}
+          //   duration={500}
+          // >
+          //   <button className="font-rubik hover:text-[#f68b09] text-xl">Work</button>
+          // </Link>
+          // <Link
+          //   activeClass="text-yellow-400"
+          //   to="contact"
+          //   spy={true}
+          //   smooth={true}
+          //   offset={-70}
+          //   duration={500}
+          // >
+          //   <button className="font-rubik hover:text-[#ff6150] text-xl">Contact</button>
+          // </Link>
 //           <a
 //             href="https://drive.google.com/file/d/151Z47917h6Q005Y7536316574q17838/view?usp=sharing"
 //             target="_blank"
 //             rel="noreferrer"
-//             className="bg-blue-400 hover:bg-blue-300 text-gray-900 px-4 py-2 rounded-md"
+//             className=" text-xl bg-[#ffffff] hover:bg-[#bfc8d3] text-[#272643] px-3 py-1.5 rounded-lg font-rubik"
 //           >
-          
 //             Download CV
 //           </a>
+          
 //         </div>
 //       </nav>
 //     </header>
+    
 //   );
 // };
 
 // export default Header;
 
 
+
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 
-
-
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
-    <header  className="py-5 px- rounded-xl   bg-[#04192f] mr-4">
+    <header className="py-5  rounded-xl bg-[#04192f]  ">
       <nav className="flex items-center justify-between">
         <div className="flex items-center">
           <Link
@@ -90,7 +107,7 @@ const Header = () => {
             <span className="text-white text-4xl font-bold font-rubik ml-4">Phuris</span>
           </Link>
         </div>
-        <div className="flex items-center space-x-7 ">
+        <div className="hidden md:flex items-center space-x-7 mr-4">
           <Link
             activeClass="text-yellow-400"
             to="about"
@@ -139,17 +156,98 @@ const Header = () => {
           >
             Download CV
           </a>
-          
         </div>
+        {/* Responsive Toggle Button */}
+        <button
+          onClick={toggleNav}
+          className="md:hidden text-white focus:outline-none"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+        {/* Responsive Nav Menu */}
+        {isNavOpen && (
+          <div className="md:hidden absolute top-16 right-0 bg-[#04192f] p-4 space-y-4  ">
+            <ul>
+              <li className="mb-1">
+                <Link
+                  activeClass="text-yellow-400"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  <button className="font-rubik hover:text-[#54d2d2] text-xl">About</button>
+                </Link>
+              </li>
+              <li className="mb-1">
+                <Link
+                  activeClass="text-yellow-400"
+                  to="skills"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  <button className="font-rubik hover:text-[#ffcb00] text-xl">Skills</button>
+                </Link>
+              </li>
+              <li className="mb-1">
+                <Link
+                  activeClass="text-yellow-400"
+                  to="work"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  <button className="font-rubik hover:text-[#f68b09] text-xl">Work</button>
+                </Link>
+              </li>
+              <li className="mb-3">
+                <Link
+                  activeClass="text-yellow-400"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  <button className="font-rubik hover:text-[#ff6150] text-xl">Contact</button>
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/151Z47917h6Q005Y7536316574q17838/view?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                  className=" text-xl bg-[#ffffff] hover:bg-[#bfc8d3] text-[#272643] px-3 py-1.5 rounded-lg font-rubik"
+                >
+                  Download CV
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     </header>
-    
   );
 };
 
 export default Header;
-
-
 
 
 
