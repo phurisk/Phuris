7b0721f2cf8bcaf12effd1692f8ae31c94b31e41
Phuris Kruacharee <137043070+phurisk@users.noreply.github.com>
2023-12-24 23:37:17 +0700

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -53,29 +53,29 @@ const Contact = () => {
             </svg>
             Phone
           </a>
-          <a
-            href="https://www.facebook.com/PhurisKR"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center w-full p-4 text-gray-400 hover:text-[#ff6150] rounded-md"
-          >
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
-              />
-            </svg>
-            Facebook
-          </a>
+          // <a
+          //   href="https://www.facebook.com/PhurisKR"
+          //   target="_blank"
+          //   rel="noopener noreferrer"
+          //   className="flex items-center justify-center w-full p-4 text-gray-400 hover:text-[#ff6150] rounded-md"
+          // >
+          //   <svg
+          //     fill="none"
+          //     stroke="currentColor"
+          //     strokeWidth="1.5"
+          //     viewBox="0 0 24 24"
+          //     xmlns="http://www.w3.org/2000/svg"
+          //     aria-hidden="true"
+          //     className="w-6 h-6 mr-2"
+          //   >
+          //     <path
+          //       strokeLinecap="round"
+          //       strokeLinejoin="round"
+          //       d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+          //     />
+          //   </svg>
+          //   Facebook
+          // </a>
           {/* Other contact options */}
         </div>
       </div>
